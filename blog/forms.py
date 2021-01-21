36d8570f8ab349a2cb1d3e04b02cb8244fb99b60
thabo <thabothibosindlovu@gmail.com>
2021-01-22 01:17:34 +0200

    add category navlink on blog pages

--- blog/forms.py
+++ blog/forms.py
@@ -22,9 +22,10 @@ class PostForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
 	class Meta:
 		model = Post
-		fields = ('title', 'body')
+		fields = ('title', 'category', 'body')
 		
 		widgets = {
 		    'title': forms.TextInput(attrs={'class': 'form-control'}),
+		    'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
 		    'body': forms.Textarea(attrs={'class': 'form-control'}),
 		}
--- blog/views.py
+++ blog/views.py
@@ -13,15 +13,29 @@ class HomeView(ListView):
 	template_name = 'home.html'
 	ordering = ['-post_date']
 	#ordering = ['-id']
-
+	
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(HomeView, self).get_context_data(*args, **kwargs)
+		context['cat_menu'] = cat_menu
+		return context
+	   
 def CategoryView(request, cats):
-	category_posts = Post.objects.filter(category=cats.replace('-',' '))
-	context = {'cats':cats.title().replace('-',' '), 'category_posts':category_posts}
+	category_posts = Post.objects.filter(category=cats.replace('-',' ')).order_by('-post_date')
+	cat_menu = Category.objects.all()             # so as to render the category nav-link in this page
+	context = {'cats':cats.title().replace('-',' '), 'category_posts':category_posts, 'cat_menu':cat_menu}
 	return render(request, 'categories.html', context)
+	
 
 class ArticleView(DetailView):
 	model = Post
 	template_name = 'detail_view.html'
+	
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(ArticleView, self).get_context_data(*args, **kwargs)
+		context["cat_menu"] = cat_menu
+		return context
 
 class AddPostView(CreateView):
 	model = Post
@@ -38,8 +52,16 @@ class EditPostView(UpdateView):
 	model = Post
 	form_class = EditForm			#fields are specified in the form
 	template_name = 'update_post.html'
+	
+	def get_context_data(self, *args, **kwargs):
+		cat_menu = Category.objects.all()
+		context = super(EditPostView, self).get_context_data(*args, **kwargs)
+		context["cat_menu"] = cat_menu
+		return context
 
 class DeletePostView(DeleteView):
 	model = Post
 	template_name = 'delete_post.html'
 	success_url = reverse_lazy('home')
+	
+	
