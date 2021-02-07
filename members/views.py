39c0ed2313e75239fd858852ec9c6f01e82f1fa7
thabo <thabothibosindlovu@gmail.com>
2021-02-07 14:01:17 +0200

    Profile view page

--- members/views.py
+++ members/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm
+from blog.models import Profile
 
 # Create your views here.
 class UserRegisterView(generic.CreateView):
@@ -26,3 +28,16 @@ class PasswordsChangeView(PasswordChangeView):
 
 def password_success(request):
 	return render(request, 'registration/password_changed.html', {})
+	
+class ShowProfilePageView(DetailView):
+	model = Profile
+	template_name = 'registration/user_profile.html'
+	
+	def get_context_data(self, *args, **kwargs):
+		#users = Profile.objects.all()
+		context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+		
+		page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+		
+		context["page_user"] = page_user
+		return context
